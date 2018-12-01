4a5afc11bb5082007f2ca9d307d0ba12f8fc88b4
fw6 <lyman.von9506@gmail.com>
2018-12-01 13:54:45 +0800

    777

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
 import React from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import './app.styl'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // show: true
+      list: []
+    }
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      // show: this.state.show ? false : true
+      list: [...prevState.list, 'item']
+    }))
+  }
+
   render() {
     return (
       <>
-        <h1>React Animation</h1>
-        <button>Toggle</button>
+        {/* <CSSTransition
+          in={this.state.show}
+          timeout={1000}
+          classNames="fade"
+          unmountOnExit
+          onEntered={el => {
+            el.style.color = 'blue'
+          }}
+          appear={true}
+        >
+          <h1>React Animation</h1>
+        </CSSTransition> */}
+        <TransitionGroup>
+          {this.state.list.map((item, index) => {
+            return (
+              <CSSTransition
+                timeout={1000}
+                classNames="fade"
+                unmountOnExit
+                onEntered={el => {
+                  el.style.color = 'blue'
+                }}
+                appear={true}
+                key={index}
+              >
+                <div>{item}</div>
+              </CSSTransition>
+            )
+          })}
+        </TransitionGroup>
+        <button onClick={() => this.handleClick()}>Toggle</button>
       </>
     )
   }
